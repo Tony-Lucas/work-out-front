3a583e3d41d4c8d2b56d7526c5ccef781d4f271f
Tony Lucas <tonylucas1997@hotmail.com>
2022-06-27 15:33:21 -0300

    icone options

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -27,3 +27,13 @@ export const LoadIcon: FunctionComponent<IconAttributes> = ({ width, height, col
         </svg>
     )
 }
+
+export const OptionsIcon: FunctionComponent<IconAttributes> = ({ width, height, color, stroke }) =>{
+    return(
+        <svg width={width || "20"} height={height || "20"} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.99999 16.6667C10.4602 16.6667 10.8333 16.2936 10.8333 15.8333C10.8333 15.3731 10.4602 15 9.99999 15C9.53975 15 9.16666 15.3731 9.16666 15.8333C9.16666 16.2936 9.53975 16.6667 9.99999 16.6667Z" fill="#333333" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9.99999 10.8333C10.4602 10.8333 10.8333 10.4602 10.8333 9.99999C10.8333 9.53975 10.4602 9.16666 9.99999 9.16666C9.53975 9.16666 9.16666 9.53975 9.16666 9.99999C9.16666 10.4602 9.53975 10.8333 9.99999 10.8333Z" fill="#333333" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9.99999 5.00001C10.4602 5.00001 10.8333 4.62691 10.8333 4.16668C10.8333 3.70644 10.4602 3.33334 9.99999 3.33334C9.53975 3.33334 9.16666 3.70644 9.16666 4.16668C9.16666 4.62691 9.53975 5.00001 9.99999 5.00001Z" fill="#333333" stroke="#333333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
